b4c6e3072d907d6086008b1d72b80ceca1f34b33
Rohit Shrivastav <rohitrks805@gmail.com>
2024-03-17 23:46:16 +0530

    Home setup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Home() {
-    // const options = {
-    //     method: 'GET',
-    //     url: 'https://movies-api14.p.rapidapi.com/movies',
-    
-    //     headers: {
-    //     'X-RapidAPI-Key': import.meta.env.RAPIDAPI_KEY,
-    //     'X-RapidAPI-Host': import.meta.env.RAPIDAPI_HOST 
-    //   },
-    
-    //   }
-    
-    //   try {
-    //     const data = axios.request(options)
-    //     .then((res)=>{
-    //       console.log(res.data);
-    //     })
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
+  const [data, setData] = useState([])
+  const [trendingMovies, setTrendingMovies] = useState([])
+    const options = {
+      method: 'GET',
+      url: 'https://movies-api14.p.rapidapi.com/home',
+      headers: {
+        'X-RapidAPI-Key': 'c2d4af3ce4mshd3561860489b6c5p10c016jsn84add2f23123',
+        'X-RapidAPI-Host': import.meta.env.RAPIDAPI_HOST
+      }
+    };
+
+    useEffect(()=>{
+
+    try {
+      axios.request(options)
+      .then((res)=> {
+        setData(res.data)
+        // Trending Movies 
+        // setTrendingMovies(res.data[0].movies)
+        // console.log(res.data);
+        console.log(res.data[0].movies);
+        setTrendingMovies(res.data[0].movies)
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  })
+
+
   return (
-    <div className='h-screen bg-slate-400'>Home</div>
+    <div className='h-screen bg-slate-400'>
+      {
+        trendingMovies.map(item => (
+          <div key={item.title}>
+            <h1>{item._id}</h1>
+            <div>
+              <h1>{item.title}</h1>
+            </div>
+
+          </div>
+          
+        ))
+      }
+    </div>
   )
 }
 
