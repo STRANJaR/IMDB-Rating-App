6d121c831d214eac8c68d0c8a4f8e218910e7bf9
Rohit Shrivastav <rohitrks805@gmail.com>
2024-03-18 23:53:31 +0530

    another api implimetnted 250 movies

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,26 +4,24 @@ import { useEffect, useState } from "react";
 function Home() {
   const [data, setData] = useState([])
   const [trendingMovies, setTrendingMovies] = useState([])
-    const options = {
-      method: 'GET',
-      url: 'https://movies-api14.p.rapidapi.com/home',
-      headers: {
-        'X-RapidAPI-Key': 'c2d4af3ce4mshd3561860489b6c5p10c016jsn84add2f23123',
-        'X-RapidAPI-Host': import.meta.env.RAPIDAPI_HOST
-      }
-    };
 
+  const options = {
+    method: 'GET',
+    url: 'https://moviesverse1.p.rapidapi.com/top-250-movies',
+    headers: {
+      'X-RapidAPI-Key': 'c2d4af3ce4mshd3561860489b6c5p10c016jsn84add2f23123',
+      'X-RapidAPI-Host': 'moviesverse1.p.rapidapi.com'
+    }
+  };
+  
     useEffect(()=>{
 
     try {
       axios.request(options)
       .then((res)=> {
         setData(res.data)
-        // Trending Movies 
-        // setTrendingMovies(res.data[0].movies)
-        // console.log(res.data);
-        console.log(res.data[0].movies);
-        setTrendingMovies(res.data[0].movies)
+        console.log(res.data.movies);
+        setTrendingMovies(res.data.movies)
       })
     } catch (error) {
       console.log(error);
@@ -36,7 +34,7 @@ function Home() {
       {
         trendingMovies.map(item => (
           <div key={item.title}>
-            <h1>{item._id}</h1>
+            {/* <h1>{}</h1> */}
             <div>
               <h1>{item.title}</h1>
             </div>
